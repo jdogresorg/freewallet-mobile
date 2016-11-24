7e8c51f50d87727d8f1107cf08241d17ee2cab2d
J-Dog <j-dog@j-dog.net>
2016-11-24 13:01:04 -0800

    spinner updates
    - allow specifying decimal precision
    - enforce number formatting
    - use simple ‘tel’ keyboard on mobile devices

--- app/view/field/Spinner.js
+++ app/view/field/Spinner.js
@@ -9,27 +9,51 @@ Ext.define('FW.view.field.Spinner', {
     
     config : {
         component: {
-            // Change type to number since it shows numbers keyboard, and allows for additional chars like ,
-            type: 'number',
+            type: 'tel',    // Use 'tel' so we bring up simple number keyboard
             disabled: false
         },
-        divisible: false // Flag to indicate if the amount is divisible
+        decimalPrecision: 0, // default to 0 (integers only)
+        listeners: {
+            // when user blurs field, re-set value to enforce formatting
+            blur: function(cmp){
+                cmp.setValue(cmp.getValue());
+            }            
+        }
     },
-    
 
-    // Override default applyValue function to force number to be in correct display format
-    applyValue : function(value, oldValue){
+
+    // Initialize the component and some values
+    initialize: function(){
         var me  = this,
-            fmt = (me.divisible) ? '0,0.00000000' : '0,0',
-            val = numeral(value).format(fmt);
-        return val
+            cfg = me.config;
+        me.decimalPrecision = cfg.decimalPrecision;
+        me.callParent();
+    },
+
+
+    // Force numbers to display in friendly format with commas
+    applyValue : function(value, oldValue){
+        var me = this;
+        return numeral(value).format(me.getNumberFormat())
+    },
+
+
+    // Handle getting display format
+    getNumberFormat: function(){
+        var me  = this,
+            fmt = '0,0';
+        if(me.decimalPrecision){
+            fmt += '.';
+            for(var i=0;i<me.decimalPrecision;i++)
+                fmt += '0';
+        }
+        return fmt;
     },
 
 
     // Override default spin function to force numeric values
     spin: function(down){
         var me        = this,
-            format    = (me.divisible) ? '0.00000000' : '0',
             origValue = me.getComponent().getValue(),
             stepValue = me.getStepValue(),
             direction = down ? 'down' : 'up',
@@ -48,20 +72,19 @@ Ext.define('FW.view.field.Spinner', {
         if(value>maxValue)
             value = maxValue;
         // Set value and fire off spin events
-        value = value.format(format);
         me.setValue(value);
         me.fireEvent('spin', me, value, direction);
         me.fireEvent('spin' + direction, me, value);
     },
 
 
-    // Handle setting flag to determine if value is divisible
-    setDivisible: function(divisible){
-        var me  = this,
-            div = (divisible) ? true : false;
-        me.divisible = div;
+    // Handle changing decimal precision
+    setDecimalPrecision: function(val){
+        var me  = this;
+        me.decimalPrecision = val;
         me.setValue(me.getValue());
     },
+
 
     getValue: function(){
         var me = this;
